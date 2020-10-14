e7c419e5e6324055229c2320600406b500bd7896
Co Nhan <conhanq@gmail.com>
2020-10-13 23:50:05 -0700

    day2 solution

---
+++ solutions/day-02/index.js
@@ -0,0 +1,17 @@
+const title = <h2>Getting Started React </h2>
+const header = (
+  <header>
+    <h1>Welcome to 30 Days Of React</h1>
+    <h2>Getting Started React</h2>
+    <h3>JavaScript library</h3>
+  </header>
+)
+const header = (
+  <header>
+    <h1>Welcome to 30 Days Of React</h1>
+    <h2>Getting Started React</h2>
+    <h3>JavaScript Library</h3>
+    <p>Co Nhan</p>
+    <small>Oct 2, 2020</small>
+  </header>
+)
