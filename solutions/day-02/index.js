7bc3f6859e91c8d358cf503579abda0702985650
Co Nhan <conhanq@gmail.com>
2020-10-18 12:19:41 -0700

    day2-completed

--- solutions/day-02/index.js
+++ solutions/day-02/index.js
@@ -1,17 +1,51 @@
 const title = <h2>Getting Started React </h2>
+
+
+const welcome = 'Welcome to 30 Days Of React'
+const subtitle = 'Javascript Library'
+const authorFirstName = 'Co'
+const authorLastName = 'Nhan' 
+const date = 'Oct 2, 2020'
+
 const header = (
   <header>
-    <h1>Welcome to 30 Days Of React</h1>
-    <h2>Getting Started React</h2>
-    <h3>JavaScript library</h3>
+    <div className='header-wrapper'>
+    <h1>{welcome}</h1>
+    <h2>{title}</h2>
+    <h3>{subtitle}</h3>
+    <p>
+      Instructor: {authorFirstName} {authorLastName}
+    </p>
+<small>Date: {date}</small>
+    </div>
   </header>
 )
-const header = (
-  <header>
-    <h1>Welcome to 30 Days Of React</h1>
-    <h2>Getting Started React</h2>
-    <h3>JavaScript Library</h3>
-    <p>Co Nhan</p>
-    <small>Oct 2, 2020</small>
-  </header>
+
+
+// main 
+const tech = ['HTML', 'CSS', 'Javascript']
+const main = (
+  <main>
+    <p>Prerequisite to get started react.js</p>
+    <ul>
+      <li>HTML</li>
+      <li>CSS</li>
+      <li>JavaScript</li>
+    </ul>
+  </main>
+  
+)
+
+const footer = (
+  <footer>
+    <p>Copyright 2020</p>
+  </footer>
+)
+
+const app = (
+  <div>
+    {header}
+    {main}
+    {footer}
+  </div>
 )
