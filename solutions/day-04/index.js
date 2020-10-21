d744456cc7b80334d310fd762dda10285a0b73d8
Co Nhan <conhanq@gmail.com>
2020-10-21 08:07:00 -0700

    Day4-Component

--- solutions/day-04/index.js
+++ solutions/day-04/index.js
@@ -2,14 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import road from './images/road.jpg'
 
-const welcome = 'Welcome to 30 Days of React'
-const title = 'Getting Started React'
 
 const Header = () => (
     <header>
         <div className='jeader-wrapper'>
-            <h1>{welcome}</h1>
-            <h2>{title}</h2>
+            <h1>Welcome to 30 Days Of React</h1>
+            <h2>Getting Started React</h2>
             <h3>JavaScript Library</h3>
             <p>Co Nhan</p>
             <small>Oct 4, 2020</small>
@@ -35,27 +33,8 @@ const TechList = () => {
     return techsFormatted
 }
 
-// button
-const buttonStyles = {
-  padding: '10px 20px',
-  background: 'rgb(0,255,0)',
-  border: 'none',
-  borderRadius: 5,
-}
 
-const Button = () => <button style = {buttonStyles}> action </button>
 
-const hexaColor = () => {
-  let str = '0123456789abcdef'
-  let color = ''
-  for (let i = 0; i < 6; i++) {
-    let index = Math.floor(Math.random() * str.length)
-    color += str[index]
-  }
-  return '#' + color
-}
-
-const HexaColor = () => <div>{hexaColor()}</div>
 
 
 // Main Component
@@ -76,7 +55,7 @@ const Main = () => (
 
 const Footer = () => (
     <footer>
-        <div className = 'footer-wrapper'>
+        <div className = 'footer-wrapper'> 
         <p>Cppyright 2020</p>
         </div>
     </footer>
@@ -87,8 +66,6 @@ const App = () => (
   <div className ='app'>
     <Header />
     <Main />
-    <Button />
-    <HexaColor />
     <Footer />
    </div>
 )
